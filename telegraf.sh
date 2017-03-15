c864137636f7c084a68c59df91c35461ac4ab5a2
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-03-16 01:08:28 +1000

    Added telegraf agent

---
+++ telegraf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "==> Checking telegraf instance"
+INSTANCE=telegraf
+if [ ! "$(docker ps -q -f name=$INSTANCE)" ]; then
+    echo "==> No $INSTANCE instance found, starting..."
+    if [ "$(docker ps -aq -f status=exited -f name=$INSTANCE)" ]; then
+        docker rm $INSTANCE
+    fi
+    docker run -d --name telegraf \
+        --hostname=$HOSTNAME \
+        -e "HOST_SYS=/rootfs/sys" \
+        -e "HOST_ETC=/rootfs/etc" \
+        -v $PWD/telegraf/telegraf_agent.conf:/etc/telegraf/telegraf.conf:ro \
+        -v /var/run/docker.sock:/var/run/docker.sock:ro \
+        -v /sys:/rootfs/sys:ro \
+        -v /etc:/rootfs/etc:ro \
+        --link influxdb \
+        telegraf
+fi
