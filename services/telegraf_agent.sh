b2b26ecf4de4589bf37b21b047aa928d99e82f2a
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-03-22 16:48:16 +1000

    Added telegraf agent script

---
+++ services/telegraf_agent.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "==> Updating local telegraf docker image"
+docker pull telegraf
+
+echo "==> Checking telegraf instance"
+INSTANCE=telegraf
+if [ ! "$(docker ps -q -f name=$INSTANCE)" ]; then
+    echo "==> No $INSTANCE instance found, starting..."
+    if [ "$(docker ps -aq -f status=exited -f name=$INSTANCE)" ]; then
+        docker rm $INSTANCE
+    fi
+    docker run -d --name telegraf \
+        --hostname=$HOSTNAME \
+        -e "HOST_SYS=/rootfs/sys" \
+        -e "HOST_ETC=/rootfs/etc" \
+        -v $( dirname `pwd`)/etc/telegraf/telegraf_agent.conf:/etc/telegraf/telegraf.conf:ro \
+        -v /var/run/docker.sock:/var/run/docker.sock:ro \
+        -v /sys:/rootfs/sys:ro \
+        -v /etc:/rootfs/etc:ro \
+        --restart=always \
+        telegraf
+        #-e "HOST_PROC=/rootfs/proc" \
+        #-v /proc:/roots/proc:ro \
+fi
