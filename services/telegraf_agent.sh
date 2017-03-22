4d24c2a478017cd50c55077077b37260e4ee1351
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-03-22 19:51:36 +1000

    Updated hostname convention

--- services/telegraf_agent.sh
+++ services/telegraf_agent.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "==> Checking telegraf instance"
-TELEGRAF_HOSTNAME=ewe-ecs-agent.$EXP_ENVIRONMENT.$AWS_REGION.$CLUSTER_NAME.$HOSTNAME
+TELEGRAF_HOSTNAME=ewe-ecs-agent.$CLUSTER_NAME.$HOSTNAME
 INSTANCE=telegraf
 if [ ! "$(docker ps -q -f name=$INSTANCE)" ]; then
     echo "==> No $INSTANCE instance found, starting..."
@@ -8,13 +8,13 @@ if [ ! "$(docker ps -q -f name=$INSTANCE)" ]; then
         docker rm $INSTANCE
     fi
     docker run -d --name telegraf \
-        --hostname=$TELEGRAF_HOSTNAME \
-        -e "HOST_SYS=/rootfs/sys" \
-        -e "HOST_ETC=/rootfs/etc" \
+        --hostname="$TELEGRAF_HOSTNAME" \
+        -e HOST_ETC=/rootfs/etc \
+        -e HOST_SYS=/rootfs/sys \
         -e OUTPUTS_INFLUXDB_URLS='["http://10.38.95.206:8086"]' \
         -v /var/run/docker.sock:/var/run/docker.sock:ro \
-        -v /sys:/rootfs/sys:ro \
         -v /etc:/rootfs/etc:ro \
+        -v /sys:/rootfs/sys:ro \
         --restart=always \
         pedbarbosa/docker-telegraf:1.2.1
 fi
